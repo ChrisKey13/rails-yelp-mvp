7b70d25ea043bd7d6020f6056ad9dc91af8876df
christophe bouquet <christophe.productowner@gmail.com>
2019-05-20 15:07:55 +0200

    troisieme exercice

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:create, :new]
 
   def new
     # we need @restaurant in our `simple_form_for`
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    # we need `restaurant_id` to asssociate review with corresponding restaurant
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese',
+  },
+  {
+    name:         'alicheur',
+    address:      '7 rue Oberkampf',
+    category:  'french',
+  },
+  {
+    name:         'Big Mamma',
+    address:      'Station F',
+    category:  'italian',
+  },
+  {
+    name:         'La merguez qui vole',
+    address:      '292 rue Saint-Martin',
+    category:  'japanese',
+  },
+  {
+    name:         'La bonne frifrite',
+    address:      'Starsbourg saint-denis',
+    category:  'belgian',
+  },
+  {
+    name:         'Chez wong',
+    address:      '7 rue gravilliers',
+    category:  'chinese',
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
