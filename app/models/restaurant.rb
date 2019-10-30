eb12b2031eb485193d1d71fdbea2daa8f574b630
christophe bouquet <christophe.productowner@gmail.com>
2019-10-30 09:02:26 +0100

    rails new

--- app/models/restaurant.rb
+++
@@ -1,6 +0,0 @@
-class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
-  validates :name, presence: true, allow_blank: false
-  validates :address, presence: true, allow_blank: false
-end
